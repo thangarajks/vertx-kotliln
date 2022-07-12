48c10d0abc11c281588b60bdea0c6b62a04b4704
Thangaraj S <thangarajks@hotmail.co.uk>
2022-07-12 22:16:01 +0530

    Added logs

--- src/main/kotlin/learn/thangs/rest/api/controller/Controller.kt
+++ src/main/kotlin/learn/thangs/rest/api/controller/Controller.kt
@@ -3,6 +3,10 @@ package learn.thangs.rest.api.controller
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 
+/**
+* controller interface defining base methods a controller should implement. 
+* Controller provides a router which is responsible for routing requests to defined respective handlers
+*/
 interface Controller {
     fun getRouter(vertx: Vertx): Router
 }
